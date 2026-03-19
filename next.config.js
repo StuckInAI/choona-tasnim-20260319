742a66833f6094ec646ad4a2ce5bba24080615f2
kfarooq1981 <148969272+kfarooq1981@users.noreply.github.com>
2026-03-19 22:24:52 +0500

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone', // Recommended for Docker production builds
+}
+
+module.exports = nextConfig
